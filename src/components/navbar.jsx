e8d004892b72fd0b330b2a3b6ee75aba0ae38503
Sudipta Shaw <sudipthere@gmail.com>
2024-01-16 09:44:28 +0530

    2nd commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,46 +1,43 @@
 import React from "react";
-import {Link} from 'react-router-dom'
-
+import logo from "../../images/logo.png";
 import search from "../../images/search-solid.svg";
-import './Navbar.css'
+import "./navbar.css";
 import bars from "../../images/bars-solid.svg";
-import logo from '../../images/logo.png'
-
-
-
-
-
-
 
 const Navbar = () => {
-  // Uppercase "Navbar"
   return (
-    <nav>
-      <div className="navbar">
-        <button to="/" className="nav-logo-hem">
-          <img src={bars} alt="logo" style={{ width: 15 }} />
-        </button>
-        <div className="nav-section">
-          <div className="nav-logo">
-            <img src={logo} alt="logo" />
+    <>
+      <nav className="main-nav">
+        <div className="navbar">
+          <button className="slide-in-icon">
+            <img src={bars} alt="bars" width="15" />
+          </button>
+          <div className="navbar-1">
+            <div to="#" className="nav-item nav-logo">
+              <img src={logo} alt="logo" />
+            </div>
+            <form>
+              <input type="text" id="search" placeholder="Search..." />
+              <img
+                src={search}
+                alt="search"
+                width="18"
+                className="search-icon"
+              />
+            </form>
+            <div to="#" className="nav-item nav-btn res-nav">
+              About
+            </div>
+            <div to="#" className="nav-item nav-btn res-nav">
+              Products
+            </div>
+            <div to="#" className="nav-item nav-btn res-nav">
+              For Teams
+            </div>
           </div>
-          <form action="">
-            <input type="text" placeholder="Search " name="Search..." />
-            <img src={search} alt="search" className="search-icon" width='18' />
-          </form>
-
-          <Link to="/" className="nav-item btn">
-            About
-          </Link>
-          <Link to="/" className="nav-item btn">
-            Products
-          </Link>
-          <Link to="/" className="nav-item btn">
-            For Teams
-          </Link>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
